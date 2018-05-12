2305ce41b575fb77d1ef73bb2260131d8849984a
JaehunYoon <goodasd123@gmail.com>
2018-05-12 20:39:08 +0900

    PyQT - 02 Signal, Slot connect

--- Examples/02 Signal and Slot/signal_and_slot.py
+++ Examples/02 Signal and Slot/signal_and_slot.py
@@ -30,3 +30,12 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     w = Form()
     sys.exit(app.exec())
+
+
+'''
+Signal과 Slot은 위젯끼리 통신을 하는 용도로 사용할 수 있습니다.
+어떠한 이벤트가 일어났을때 그것을 다른 오브젝트에게 알려주고 값을 전달하거나 받을 수 있도록 해줍니다.
+콜백 함수등을 이용하는 다른 GUI 프레임웍과 비교되는 통신 방법으로 Qt의 강점이라 할 수 있습니다.
+
+QObject 를 상속받은 모든 위젯은 Signal과 Slot을 가질 수 있습니다.
+'''
